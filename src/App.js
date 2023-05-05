893b3446cf8da9af4ec4ac8d6992239a1bbdf843
premkumar8686 <premkumarweb0369@gmail.com>
2023-05-05 18:17:46 +0530

    First Commite

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useMemo, useState } from "react";
 
 function App() {
+
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+
+  const increment = () => {
+    setCount(count + 1);
+  }
+
+
+ const addTodo = () => {
+  setTodos([...todos, {id: '1',Name: 'ToDo'}])
+ }
+
+ const expensiveCalculation = (num) => {
+    console.log("Calculating...");
+    for(let i = 0; i <= 1000000000; i++ )
+    {
+      num += 1;
+    }
+    return num;
+  };
+
+ //const calculation = expensiveCalculation(1);
+//  const calculation = useMemo(()=> expensiveCalculation(1),[]);
+ const calculation = useMemo(()=> expensiveCalculation(count) ,[count]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <h1>{count}</h1>
+     <button onClick={increment}>Increment</button>
+
+     <ul>
+        {todos.map((ele,keys) => {
+          return <li key={keys}>{ele.Name}</li>
+        })}
+     </ul>
+
+     <button onClick={addTodo}>Add ToDo</button>
+
+     <h2>Expensive Calculation</h2>
+        {calculation}
+    </>
   );
-}
+
+
+};
 
 export default App;
